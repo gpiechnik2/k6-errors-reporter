167c210a80253a084f48d5bea1ecdf4f7bb6e575
gpiechnik2 <grzegorz.piechnik99@gmail.com>
2022-10-07 02:34:31 +0200

    Webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,12 @@ const path = require('path');
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
 module.exports = {
+  mode: 'production',
   entry: './src/reporter.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    libraryTarget: "commonjs"
   },
   plugins: [
     new NodePolyfillPlugin()
