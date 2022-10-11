843667a0a86ba28151002349cc2b5691dcaed5b1
gpiechnik2 <grzegorz.piechnik99@gmail.com>
2022-10-11 21:27:34 +0200

    Logs style added

--- src/reporter.js
+++ src/reporter.js
@@ -1,22 +1,17 @@
 import getErrorCodeMessage from "../utils/getErrorCodeMessage"
 
 export function checkResponse(res, status, log = false) {
-    console.log(log)
-    console.log(status)
-    
     if (!status) {
-        console.log("://")
         if (log) {
-            console.log(":))")
             console.error(res)
         } else {
-            console.log("gucci")
             console.error(
-                "status.....: " + res.status +
-                "\nerror_code.: " + getErrorCodeMessage(res.error_code) +
-                "\nerror......: " + res.error +
-                "\nstatus_text: " + res.status_text +
-                "\nrequest....: " + res.request.method + " " + res.request.url + "\n"
+                "\n  status.....: " + res.status +
+                "\n  error_code.: " + getErrorCodeMessage(res.error_code) +
+                "\n  error......: " + res.error +
+                "\n  status_text: " + res.status_text +
+                "\n  request....: " + res.request.method + " " + res.request.url +
+                "\n "
             )
         }
     }
